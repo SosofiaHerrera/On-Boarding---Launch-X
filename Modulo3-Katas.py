3b5e794c1004b0b1430514ff1ae7fb08de74b752
Sofia Herrera <76921444+SosofiaHerrera@users.noreply.github.com>
2022-02-18 10:16:23 -0600

    Delete Modulo3-Katas.py

--- Modulo3-Katas.py
+++
@@ -1,7 +0,0 @@
-velocidad = input("Ingrese la velocidad del asteroide: ")
-asteroide = 49
-
-if asteroide > 25:
-    print("\n¡ADVERTENCIA! Se acerca un asteroide")
-else:
-    print("\nTodo está bien:)")
